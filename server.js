591dc5512b38845bccf49ca571d70566ab4e484a
diaa hussein <diaahussein110@gmail.com>
2025-05-08 00:10:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,43 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Serve static files from the current directory
 app.use(express.static(path.join(__dirname)));
+
+// Store connected users and their associated groups
+const userGroups = {};
 
 io.on('connection', (socket) => {
   console.log('A user connected');
-  socket.on('buzz', () => {
-    socket.broadcast.emit('buzz'); // or `io.emit('buzz')` to send to all
+
+  // When a user joins a group
+  socket.on('joinGroup', (groupName) => {
+    console.log(`User joined group: ${groupName}`);
+    if (!userGroups[groupName]) {
+      userGroups[groupName] = [];
+    }
+    userGroups[groupName].push(socket.id); // Add socket ID to the group's list
+  });
+
+  // When a buzz is triggered for a specific group
+  socket.on('buzz', (data) => {
+    const { group } = data;
+    console.log(`Buzz sent to group: ${group}`);
+    if (userGroups[group]) {
+      // Emit the buzz only to the group members
+      userGroups[group].forEach(socketId => {
+        io.to(socketId).emit('buzz'); // Send the buzz to each member in the group
+      });
+    }
+  });
+
+  // Handle disconnect event
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+    // Remove the user from all groups they were part of
+    for (let group in userGroups) {
+      userGroups[group] = userGroups[group].filter(socketId => socketId !== socket.id);
+    }
   });
 });
 
